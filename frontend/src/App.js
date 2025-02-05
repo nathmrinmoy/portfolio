5126d7cdc5655a5c394cf64a6f62c3fed3865290
Mrinmoy Nath <nathmrinmoy001@gmail.com>
2025-02-05 23:33:27 +0530

    Remove unused motion import from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter } from 'react-router-dom';
-import { motion } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
