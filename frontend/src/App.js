b57870c7fc5ea05f3d78482c3fa388c9e1a88003
nathmrinmoy <nathmrinmoy001@gmail.com>
2026-09-07 18:43:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import ProjectDetail from './pages/ProjectDetail';
 import './styles/App.scss';
 import { useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import Loader from './components/Loader';
+
+function MainPage() {
+  return (
+    <>
+      <Navbar />
+
+      <section id="home">
+        <Home />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+    </>
+  );
+}
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,25 +40,25 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <AnimatePresence mode='wait'>
+        <AnimatePresence mode="wait">
           {isLoading ? (
-            <Loader onLoadingComplete={() => setIsLoading(false)} />
+            <Loader
+              onLoadingComplete={() => setIsLoading(false)}
+            />
           ) : (
-            <>
-              <Navbar />
-              <section id="home">
-                <Home />
-              </section>
-              <section id="projects">
-                <Projects />
-              </section>
-              <section id="about">
-                <About />
-              </section>
-              <section id="contact">
-                <Contact />
-              </section>
-            </>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+
+              <Route
+                path="/projects/copilotgtm"
+                element={<ProjectDetail />}
+              />
+
+              <Route
+                path="*"
+                element={<MainPage />}
+              />
+            </Routes>
           )}
         </AnimatePresence>
       </div>
@@ -41,4 +66,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
